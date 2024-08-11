910d0e050dd2f37d7fe95c9c38fdfb353df01e0d
charlieCompton <ccompton@lclark.edu>
2024-08-10 21:10:05 -0400

    some basic header work

---
+++ hgbw/src/assets/Header.jsx
@@ -0,0 +1,16 @@
+const Header = () => {
+    return(
+        <>
+            <div className="w-screen h-screen flex justify-center bg-gray-400">
+                <div className="w-full h-20 columns-4 p-6 bg-slate-800">
+                    <div className="bg-green-400 w-full h-full"></div>
+                    <div className="bg-green-400 w-full h-full"></div>
+                    <div className="bg-green-400 w-full h-full"></div>
+                    <div className="bg-green-400 w-full h-full"></div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Header;
