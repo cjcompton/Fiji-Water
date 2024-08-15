60d0194f734d0d5111b5acbf56af1cff63fdcd9c
Charlie C. Desktop <ccompton@lclark.edu>
2024-08-15 13:28:22 -0400

    reactive header progress

--- hgbw/src/App.jsx
+++ hgbw/src/App.jsx
@@ -1,4 +1,4 @@
-import Header from "./assets/Header"
+import Header from "./components/Header"
 
 export default function App() {
   return (
--- hgbw/src/components/Header.jsx
+++ hgbw/src/components/Header.jsx
@@ -1,12 +1,19 @@
+import React from "react";
+import Logo from '../assets/Mockup_01.png';
+
 const Header = () => {
     return(
         <>
             <div className="w-screen h-screen flex justify-center bg-gray-400">
-                <div className="w-full h-20 columns-4 p-6 bg-slate-800">
+                <div className="hidden md:block w-full h-20 p-6 bg-slate-800 columns-4">
                     <div className="bg-green-400 w-full h-full"></div>
                     <div className="bg-green-400 w-full h-full"></div>
                     <div className="bg-green-400 w-full h-full"></div>
                     <div className="bg-green-400 w-full h-full"></div>
+                </div>
+                <div className="block md:hidden w-full h-20 bg-slate-800 flex justify-center items-center">
+                    <img src={Logo} alt="Logo Image" className="w-20 h-auto mx-auto"/>
+                    <button className="absolute right-4 w-8 h-8 bg-green-500"/>
                 </div>
             </div>
         </>
