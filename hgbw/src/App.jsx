dc77376fb4fba4254985a78b6ef4c0b82da1e016
Charlie C. Desktop <ccompton@lclark.edu>
2024-08-19 13:59:38 -0400

    home is looking sollid, just a slight lighting issue with mockup bottle

--- hgbw/src/App.jsx
+++ hgbw/src/App.jsx
@@ -1,7 +1,11 @@
 import Header from "./components/Header"
+import Home from "./components/Home"
 
 export default function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <Home />
+    </>
   )
 }
---
+++ hgbw/src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Waterfall from '../assets/waterfall.jpg';
+import HGBW from '../assets/hgbw_bottle.png'
+
+const Home = () => {
+    return (
+        <div className="relative w-screen h-screen flex items-center justify-center">
+            <img className="absolute inset-0 w-full h-full object-cover z-10" src={Waterfall} alt="Waterfall" />
+            <img className="absolute max-w-full max-h-full object-cover z-30" src={HGBW} alt="Product" />
+            <div className="absolute inset-0 bg-black bg-opacity-50 z-20" />
+        </div>
+    );
+};
+
+export default Home;
